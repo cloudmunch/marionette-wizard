110b9b2ea123da178a9896cb20f60018b5f7650e
vivekkodira1 <vivek@cloudmunch.com>
2016-08-17 17:10:31 +0530

    Updates template

--- demo/javascript/templates.js
+++ demo/javascript/templates.js
@@ -4,7 +4,17 @@ define(["dust", "dusthelpers"], function(dust, dust_helpers) {
     dust.register("demo-wrapper", body_0);
 
     function body_0(chk, ctx) {
-      return chk.w("<div class=\"jsonContent\"></div><div class=\"wizardView\"></div>");
+      return chk.w("<div></div><div class=\"childViewContainer\"></div>");
+    }
+    body_0.__dustBody = !0;
+    return body_0;
+  })();
+  // javascript/templates/individual-example.dust
+  (function() {
+    dust.register("individual-example", body_0);
+
+    function body_0(chk, ctx) {
+      return chk.f(ctx.get(["title"], false), ctx, "h").w(" - ").f(ctx.get(["description"], false), ctx, "h").w("<div class=\"jsonContent\"></div><div class=\"wizardView\"></div>");
     }
     body_0.__dustBody = !0;
     return body_0;
@@ -24,6 +34,27 @@ define(["dust", "dusthelpers"], function(dust, dust_helpers) {
       var rendered;
 
       dust.render("demo-wrapper", locals, function(err, result) {
+        if (typeof callback === "function") {
+          try {
+            callback(err, result);
+          } catch (e) {}
+        }
+
+        if (err) {
+          throw err
+        } else {
+          rendered = result;
+        }
+      });
+
+      return rendered;
+    }
+  });
+  define("individual-example", function() {
+    return function(locals, callback) {
+      var rendered;
+
+      dust.render("individual-example", locals, function(err, result) {
         if (typeof callback === "function") {
           try {
             callback(err, result);
@@ -61,5 +92,5 @@ define(["dust", "dusthelpers"], function(dust, dust_helpers) {
       return rendered;
     }
   });
-  return ["demo-wrapper", "json-content"];
+  return ["demo-wrapper", "individual-example", "json-content"];
 });
