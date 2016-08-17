72781156d24f1394d5d60da2996389f595e76933
vivekkodira1 <vivek@cloudmunch.com>
2016-08-17 16:32:04 +0530

    adds dust override

--- demo/javascript/main.js
+++ demo/javascript/main.js
@@ -3,6 +3,19 @@ define ( function( require ) {
 	require( "templates" );
 	var DemoWrapperView = require( "demo-wrapper-view" );
 	var Marionette = require( "backbone.marionette" );
+	var dust = require( "dust" );
+
+	Marionette.Renderer.render = function( template, data ) {
+        var html;
+
+        // Template must be compiled and in the dust cache. Recommend pre-compiling
+        // and loading the templates as scripts at app start.
+        dust.render( template, data, function( err, out ) {
+            html = out;
+        } );
+
+        return html;
+    };
 
 	var Application = new Marionette.Application( {
         container: "#backboneContainer"
