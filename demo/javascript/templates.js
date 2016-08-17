547b44abf6e1bc32b9607835a0cea25d71ad23a2
vivekkodira1 <vivek@cloudmunch.com>
2016-08-17 16:30:41 +0530

    add built template

--- demo/javascript/templates.js
+++ demo/javascript/templates.js
@@ -1,31 +1,65 @@
 define(["dust", "dusthelpers"], function(dust, dust_helpers) {
-  // javascript/templates/hello_world.dust
+  // javascript/templates/demo-wrapper.dust
   (function() {
-    dust.register("hello_world", body_0);
+    dust.register("demo-wrapper", body_0);
 
     function body_0(chk, ctx) {
-      return chk.w("<div>Hello World</div>");
+      return chk.w("<div class=\"jsonContent\"></div><div class=\"wizardView\"></div>");
     }
     body_0.__dustBody = !0;
     return body_0;
   })();
-  return function(locals, callback) {
-    var rendered;
+  // javascript/templates/json-content.dust
+  (function() {
+    dust.register("json-content", body_0);
 
-    dust.render("hello_world", locals, function(err, result) {
-      if (typeof callback === "function") {
-        try {
-          callback(err, result);
-        } catch (e) {}
-      }
+    function body_0(chk, ctx) {
+      return chk;
+    }
+    body_0.__dustBody = !0;
+    return body_0;
+  })();
+  define("demo-wrapper", function() {
+    return function(locals, callback) {
+      var rendered;
 
-      if (err) {
-        throw err
-      } else {
-        rendered = result;
-      }
-    });
+      dust.render("demo-wrapper", locals, function(err, result) {
+        if (typeof callback === "function") {
+          try {
+            callback(err, result);
+          } catch (e) {}
+        }
 
-    return rendered;
-  }
+        if (err) {
+          throw err
+        } else {
+          rendered = result;
+        }
+      });
+
+      return rendered;
+    }
+  });
+  define("json-content", function() {
+    return function(locals, callback) {
+      var rendered;
+
+      dust.render("json-content", locals, function(err, result) {
+        if (typeof callback === "function") {
+          try {
+            callback(err, result);
+          } catch (e) {}
+        }
+
+        if (err) {
+          throw err
+        } else {
+          rendered = result;
+        }
+      });
+
+      return rendered;
+    }
+  });
+  return ["demo-wrapper", "json-content"];
 });
